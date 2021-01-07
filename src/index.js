fc4c6bb1384d81c572d086347e8a9cd17833bcb1
DJ Edwards <DJ@DJ-EDWARD.local>
2021-01-07 11:41:32 -0500

    index.js and serviceWorker.js created--.babelrc created.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 //index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
