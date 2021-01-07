feb52cc65d378917bac58516e0b93fe69889c8f4
DJ Edwards <DJ@DJ-EDWARD.local>
2021-01-07 11:55:46 -0500

    starting to work on app.js, minor adjusements to app.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
-//App.js
+import React, { Component } from 'react';
+import chart from '../logos/chart.png';
+import btc from '../logos/btc.png';
+import eth from '../logos/eth.png';
+import link from '../logos/link.png';
+import ada from '../logos/ada.png';
+import xmr from '../logos/xmr.png';
+import yfi from '../logos/yfi.png';
+import lend from '../logos/lend.png';
+import comp from '../logos/comp.png';
+import uni from '../logos/uni.png';
+import gnt from '../logos/gnt.png';
+import xrp from '../logos/xrp.png';
+import snake from '../logos/snake.png';
+import coin from '../logos/coin.png';
+
+const axios = require("axios");
